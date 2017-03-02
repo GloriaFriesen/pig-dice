7c767e7e165e0425771ed6811464751fd38a7405
Cameron Jensen and Gloria Friesen <cameron@>
2017-03-02 08:22:39 -0800

    add forEach loop to add turnTotal

--- js/scripts.js
+++ js/scripts.js
@@ -24,8 +24,6 @@ function getRoll(min, max) {
 
 
 
-
-
 //User Logic
 $(document).ready(function() {
   $("#playerForm").submit(function(){
@@ -36,13 +34,19 @@ $(document).ready(function() {
 
     var roll = parseInt(getRoll(1,6));
 
+
     turns.push(roll);
     console.log(turns);
+    var totalTurn = 0
+    turns.forEach(function(turn) {
+      totalTurn += turn
+    });
+    console.log(totalTurn);
 
-    var newPlayerOne = new Player (player1, roll)
+    var newPlayerOne = new Player (player1, roll, totalTurn)
 
-    var newPlayerTwo = new Player (player2, roll)
+    var newPlayerTwo = new Player (player2, roll, totalTurn)
     console.log(newPlayerOne);
-    console.log(newPlayerTwo);     
+    console.log(newPlayerTwo);
   });
 });
